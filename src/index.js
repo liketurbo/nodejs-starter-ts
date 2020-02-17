ae0a9be6d74a8e173fbd7fd9ccf80fb8ea189b8c
Ramzan Chasygov <theliketurbo@gmail.com>
2020-02-17 17:10:42 +0300

    refac: from promise to async

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import cheerio from "cheerio"
 import request from "request-promise-native"
 
-request("https://yandex.com").then(html => {
+const start = async () => {
+  const html = await request("https://yandex.com")
+
   const $ = cheerio.load(html)
   const loc = $(".geolink__reg").text()
 
   // eslint-disable-next-line no-console
   console.log(`You in ${loc}`)
-})
+}
+
+start()
