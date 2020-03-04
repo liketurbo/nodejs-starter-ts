bc2a44fcd12363c00df00a407279e38703ec7680
Ramzan Chasygov <theliketurbo@gmail.com>
2020-03-05 00:10:14 +0300

    feat: add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,27 @@
 module.exports = {
-  extends: ["plugin:prettier/recommended", "get-off-my-lawn"],
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+    "get-off-my-lawn",
+  ],
   plugins: ["simple-import-sort"],
   rules: {
+    // fix: typescript
+    "@typescript-eslint/explicit-function-return-type": "off",
+
     // enable: eslint-plugin-simple-import-sort
     "sort-imports": "off",
     "import/order": "off",
     "simple-import-sort/sort": "error",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".ts"],
+      },
+    },
   },
 }
